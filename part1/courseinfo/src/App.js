b53efba8ac22ee77bffb380a066a341bee491627
myko <michaelluchen1998@gmail.com>
2022-02-24 14:54:10 -0500

    completed all but section d exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -4,19 +4,17 @@ const Header = ({ course }) => {
   )
 }
 
-const Content = (props) => {
-  console.log("content ",props)
+const Content = ({parts}) => {
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1} />
-      <Part part={props.part2} exercise={props.exercises2} />
-      <Part part={props.part3} exercise={props.exercises3} />
+      <Part part={parts[0].name} exercise={parts[0].exercises} />
+      <Part part={parts[1].name} exercise={parts[1].exercises} />
+      <Part part={parts[2].name} exercise={parts[2].exercises} />
     </>
   )
 }
 
 const Part = (props) => {
-  console.log(props)
   return (
     <p>
       {props.part} {props.exercise}
@@ -24,32 +22,39 @@ const Part = (props) => {
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
 
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
   )
 }
 
 
 const App = () => {
-  const course = 'Half Stack application development'
 
-  const contentProps = {
-    part1: 'Fundamentals of React',
-    exercises1: 10,
-    part2: 'Using props to pass data',
-    exercises2: 7,
-    part3: 'State of a component',
-    exercises3: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-
 
   return (
     <div>
-      <Header course={course} />
-      <Content {...contentProps} />
-      <Total exercises1={contentProps.exercises1} exercises2={contentProps.exercises2} exercises3={contentProps.exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
